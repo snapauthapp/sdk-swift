58b97d586dea9f4ccfff2d1d267a3a0f1d3f6eb1
Eric Stern <eric@ericstern.com>
2024-05-29 16:13:05 -0700

    Clean up a bunch of error handling logic

--- Sources/SnapAuth/Errors.swift
+++ Sources/SnapAuth/Errors.swift
@@ -1,40 +1,51 @@
-/* FIXME: Go through and ensure errors are complete and accurate.
-public enum AuthenticationError: Error {
-    /// The user canceled
-    case canceled
-    /// There was a network interruption
-    case networkDisrupted
-
-    case asAuthorizationError
-}
- */
-
-
 public enum SnapAuthError: Error {
-    /// The network request was disrupted.
+    /// The network request was disrupted. This is generally safe to retry.
     case networkInterruption
 
-    /// The SDK received a response from SnapAuth, but it arrived in an unexpected format. If you encounter this, please reach out to us.
+    // MARK: Internal errors, which could represent SnapAuth bugs
+
+    /// The SDK received a response from SnapAuth, but it arrived in an
+    /// unexpected format. If you encounter this, please reach out to us.
     case malformedResposne
 
-    /// The SDK was unable to encode data to send to SnapAuth. If you ever encounter this, please reach out to us.
+    /// The SDK was unable to encode data to send to SnapAuth. If you encounter
+    /// this, please reach out to us.
     case sdkEncodingError
 
-    /// The request was valid and understood, but processing was refused.
+    /// The request was valid and understood, but processing was refused. If you
+    /// encounter this, please reach out to us.
     case badRequest
 
+    // MARK: Weird responses
 
-    /// ASAuthorizationServices sent SnapAuth an unexpected type of response which we don't know how to handle. If you encounter this, please reach out to us.
+    /// ASAuthorizationServices sent SnapAuth an unexpected type of response
+    /// which we don't know how to handle. If you encounter this, please reach
+    /// out to us.
     case unexpectedAuthorizationType
 
-    /// Some of the data SnapAuth requested during credential registration was not provided, so we cannot proceed.
+    /// Some of the data SnapAuth requested during credential registration was
+    /// not provided, so we cannot proceed.
     case registrationDataMissing
 
-    // Duplicated (ish) from ASAuthorizationError
+    // MARK: Duplicated/relayed from ASAuthorizationError
+
+    /// An unknown error occurred.
     case unknown
-//    case canceled
-//    case invalidResponse
-//    case notHandled
-//    case failed
-//    case notInteractive
+
+    /// Request canceled, which can either be explicit (such as the user
+    /// canceling) or implicit (such as no matching credentials available)
+    case canceled
+
+    /// There was an invalid response from the authenticator.
+    case invalidResponse
+
+    // (Usage unknown, Apple docs are not clear - timeout?)
+    case notHandled
+
+    /// Authorization failed. This is often due to incorrect setup of Associated
+    /// Domains, or an API key that does not match the Associated Domains.
+    case failed
+
+    // (Usage unknown, Apple docs are not clear)
+    case notInteractive
 }

797aff3845d1c7d0e70307a98fdc65e135337611
Eric Stern <eric@ericstern.com>
2024-05-29 19:45:49 -0700

    Clean out generated junk and unhelpful cases

--- Tests/SnapAuthTests/Base64URLTests.swift
+++ Tests/SnapAuthTests/Base64URLTests.swift
@@ -3,28 +3,24 @@ import XCTest
 
 final class Base64URLTests: XCTestCase {
     let validBase64UrlTestCases: [(String, Data)] = [
-        ("", Data()), // empty string
-        ("Zg", "f".data(using: .utf8)!), // "f"
-        ("Zm8", "fo".data(using: .utf8)!), // "fo"
-        ("Zm9v", "foo".data(using: .utf8)!), // "foo"
-        ("Zm9vYg", "foob".data(using: .utf8)!), // "foob"
-        ("Zm9vYmE", "fooba".data(using: .utf8)!), // "fooba"
-        ("Zm9vYmFy", "foobar".data(using: .utf8)!), // "foobar"
-        ("SGVsbG8td29ybGQ", "Hello-world".data(using: .utf8)!), // "Hello-world" (no padding, - instead of +)
-        ("SGVsbG8_d29ybGQ", "Hello?world".data(using: .utf8)!), // "Hello?world" (no padding, _ instead of /)
-        ("SGVsbG8", "Hello".data(using: .utf8)!), // "Hello"
-        ("SGVsbG8gd29ybGQ", "Hello world".data(using: .utf8)!), // "Hello world"
-        ("U29tZSByYW5kb20gdGV4dA", "Some random text".data(using: .utf8)!), // "Some random text"
-        ("Zm9vYmFyCg", "foobar\n".data(using: .utf8)!), // "foobar\n" (no padding)
-//            ("Zm9vYmFyCg==", "foobar\n".data(using: .utf8)!), // "foobar\n" (padding)
-        ("MTIzNDU2Nzg5MA", "1234567890".data(using: .utf8)!), // "1234567890"
-        ("L3Vzci9iaW4vZW52Cg", "/usr/bin/env\n".data(using: .utf8)!), // "/usr/bin/env\n"
-        ("U3dpZnQgbGlrZSBzb2Z0d2FyZQo", "Swift like software\n".data(using: .utf8)!), // "Swift like software\n"
-        ("U29tZSB0ZXh0IHdpdGggb25lIHNwYWNl", "Some text with one space".data(using: .utf8)!), // "Some text with one space"
-        ("U29tZSB0ZXh0IHdpdGggdHdvIHNwYWNl", "Some text with two space".data(using: .utf8)!), // "Some text with two space"
-        ("U29tZSB0ZXh0IHdpdGggdGhyZWUgc3BhY2Vz", "Some text with three spaces".data(using: .utf8)!), // "Some text with three spaces"
-        ("AAEC", Data([0x00, 0x01, 0x02])), // binary data 00 01 02
-        ("AQIDBAUGBwgJCgsMDQ4P", Data([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F])) // binary data 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F
+        ("", Data()),
+        ("Zg", "f".data(using: .utf8)!),
+        ("Zm8", "fo".data(using: .utf8)!),
+        ("Zm9v", "foo".data(using: .utf8)!),
+        ("Zm9vYg", "foob".data(using: .utf8)!),
+        ("Zm9vYmE", "fooba".data(using: .utf8)!),
+        ("Zm9vYmFy", "foobar".data(using: .utf8)!),
+        ("SGVsbG8_d29ybGQ", "Hello?world".data(using: .utf8)!),
+        ("SGVsbG8gd29ybGQ", "Hello world".data(using: .utf8)!),
+        ("Zm9vYmFyCg", "foobar\n".data(using: .utf8)!),
+//            ("Zm9vYmFyCg==", "foobar\n".data(using: .utf8)!),
+        ("MTIzNDU2Nzg5MA", "1234567890".data(using: .utf8)!),
+        ("L3Vzci9iaW4vZW52Cg", "/usr/bin/env\n".data(using: .utf8)!),
+        ("U29tZSB0ZXh0IHdpdGggdHdvIHNwYWNl", "Some text with two space".data(using: .utf8)!),
+        ("U29tZSB0ZXh0IHdpdGggdGhyZWUgc3BhY2Vz", "Some text with three spaces".data(using: .utf8)!),
+        // Binary data
+        ("AAEC", Data([0x00, 0x01, 0x02])),
+        ("AQIDBAUGBwgJCgsMDQ4P", Data([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F])),
     ]
 
     let invalidBase64UrlStrings: [String] = [
@@ -40,15 +36,14 @@ final class Base64URLTests: XCTestCase {
         "Zm9vYmFy ", // invalid space character
         "Zm9vYmFy\t", // invalid tab character
         "Zm9vYmFy\r", // invalid carriage return character
-//        "Zm9vYmFy\v", // invalid vertical tab character
     ]
 
     func testRoundtrip() throws {
         for (base64url, data) in validBase64UrlTestCases {
-//            let fromData = Base64URL(from: data)
-//            XCTAssertEqual(fromData.string, base64url)
-//            XCTAssertEqual(fromData.data, data)
-//            XCTAssertEqual(try Base64URL(fromData.string).data, data)
+            let fromData = Base64URL(from: data)
+            XCTAssertEqual(fromData.string, base64url)
+            XCTAssertEqual(fromData.data, data)
+            XCTAssertEqual(try Base64URL(fromData.string).data, data)
 
             let fromString = try Base64URL(base64url)
             XCTAssertEqual(fromString.data, data)

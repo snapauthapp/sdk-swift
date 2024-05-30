533a8c83efc360c81ce78199dce829cc0ead087d
Eric Stern <eric@ericstern.com>
2024-05-30 10:25:52 -0700

    Guard against the last one

--- Sources/SnapAuth/SnapAuth+ASACPCP.swift
+++ Sources/SnapAuth/SnapAuth+ASACPCP.swift
@@ -4,8 +4,13 @@ import AuthenticationServices
 @available(macOS 12.0, iOS 15.0, tvOS 16.0, visionOS 1.0, *)
 extension SnapAuth: ASAuthorizationControllerPresentationContextProviding {
     public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        // FIXME: do not force unwrap
-        logger.debug("presentation anchor")
+        guard anchor != nil else {
+            // There's currently no logical path here since the three
+            // entrypoints all set the anchor, but in case more direct control
+            // paths are exposed this should prevent an unwrapping crash
+            logger.error("Presentation anchor missing, providing default")
+            return ASPresentationAnchor.default
+        }
         return anchor!
     }
 }

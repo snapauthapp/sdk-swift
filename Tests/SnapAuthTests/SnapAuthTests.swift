07da92e0be7a4ce21d776940fe266a18e2d5eee1
Eric Stern <eric@ericstern.com>
2024-05-29 19:30:13 -0700

    test ci

--- Tests/SnapAuthTests/SnapAuthTests.swift
+++ Tests/SnapAuthTests/SnapAuthTests.swift
@@ -8,5 +8,6 @@ final class SnapAuthTests: XCTestCase {
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        XCTFail("force fail")
     }
 }
